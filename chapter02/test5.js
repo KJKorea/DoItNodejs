b080e155f4f441b9c3933e9b36005e3c9e53c8a2
agassikim <agassikim@KJ-MacBook.local>
2017-03-04 23:05:55 +0900

    Chapter02-4

---
+++ chapter02/test5.js
@@ -0,0 +1,6 @@
+var calc = require('./calc');
+
+var calc2 = require('./calc2');
+
+console.log('calc.add 함수 호출 결과: %d', calc.add(10, 20));
+console.log('calc2.add 함수 호출 결과: %d', calc2.add(10, 20));
